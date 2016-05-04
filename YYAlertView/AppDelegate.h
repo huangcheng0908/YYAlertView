491bda18f7af2cd3b3b8586b8e7e235db81d664b
细雨蒙蒙飘 <huangcheng0908@163.com>
2016-05-04 15:26:54 +0800

    Initial Commit

---
+++ YYAlertView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YYAlertView
+//
+//  Created by mac on 16/5/4.
+//  Copyright © 2016年 Jack YY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YYAlertView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YYAlertView
+//
+//  Created by mac on 16/5/4.
+//  Copyright © 2016年 Jack YY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
